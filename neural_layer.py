43adb0ac9842d07d7559cdaf6e1b95513724360e
Beuren Bechlin <bfbechlin@inf.ufrgs.br>
2018-06-17 09:54:21 -0300

    Implementation of backoff propagation

--- neural_layer.py
+++ neural_layer.py
@@ -7,18 +7,28 @@ def g(x):
 G = np.vectorize(lambda x: 1 / (1 + math.exp(-x)))
 POW = np.vectorize(lambda x: x**2)
 
-class NeuralLayer:
+def _removeBias(matrix):
+        cp = np.array(matrix)
+        cp[:, 0] =  0
+        return cp
 
+class NeuralLayer:
     '''
         inputs: number of inputs neurons, does not count bias neuron
         outputs: number of outputs neurons
         thetas: initial paramaters of each feature
     '''
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         self.inputs = kwargs.get('inputs', 1)
         self.outputs = kwargs.get('outputs', 1)
         self.thetas = kwargs.get('thetas', np.random.rand(self.outputs, self.inputs + 1))
-        self.activations = np.zeros((self.outputs, 1))
+        self.reset()
+        
+    def reset(self):
+        self.a = np.zeros((self.outputs, 1))
+        self.delta = np.zeros((self.outputs, 1))
+        self.D = np.zeros((self.outputs, 1))
+        self.n = 0
 
     '''
         Compute the activations
@@ -27,14 +37,31 @@ class NeuralLayer:
     def computeActivations(self, inputs):
         x = np.vstack([1., inputs])
         z = np.dot(self.thetas, x)
-        self.activations = G(z)
-        return self.activations
+        self.a = G(z)
+        return self.a
 
     def computeRegularization(self, includeBias=False):
-        factors = POW(self.activations)
+        factors = POW(self.a)
         if not includeBias:
-            factors = np.delete(factors, 0, 1)
+            factors = _removeBias(factors)
         return factors.sum()
+
+    def computeDelta(self, deltaNL, isLast=False):
+        if isLast:
+            self.delta = self.a - deltaNL
+        else:
+            confidence = np.dot(self.a, (1 - self.a))
+            self.delta = np.dot(np.dot(np.transpose(self.thetas), deltaNL), confidence)
+        self.D = self.D + np.dot(self.delta, np.transpose(self.a))
+        self.n += 1
+        return self.delta
+
+    def updateThetas(self, ALFA, LAMBDA):
+        try:
+            D = 1 / self.n * (self.D + LAMBDA * _removeBias(self.thetas))
+        except:
+            D = 0
+        self.thetas = self.thetas - ALFA * D
 
 if __name__ == '__main__':
     a = NeuralLayer(thetas=np.array([[0.4, 0.1], [0.3, 0.2]]))
---
+++ neural_network.py
@@ -0,0 +1,14 @@
+from neural_layer import NeuralLayer
+
+
+class NeuralNetwork:
+    def __init__(self, layers):
+        self.layer = (
+            NeuralLayer(inputs=layers[i], outputs=layers[i+1]) for i in range(0, len(layers)-1)
+        )
+
+    def train(self):
+        pass
+    
+    def classify(self):
+        pass
